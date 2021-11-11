1c213a4288a9bc3c7dc9ad94b3753221fd437e5a
Matt Ware <mattbware3@gmail.com>
2021-11-11 14:44:26 +0000

    Added number generation and word loading

--- BullsAndCows/cMain.cpp
+++ BullsAndCows/cMain.cpp
@@ -1,5 +1,8 @@
 #include "cMain.h"
 #include "cSetMode.h"
+#include <random>
+#include <sstream>
+#include <fstream>
 
 cMain::cMain()
 	: wxFrame(nullptr, wxID_ANY, "Bulls and Cows", wxPoint(10, 10), wxSize(300, 400), 
@@ -17,11 +20,16 @@ cMain::cMain()
 	wxMenuItem* exit = new wxMenuItem(nullptr, wxID_ANY, wxT("&Exit\tEscape")); 
 	wxMenuItem* about = new wxMenuItem(nullptr, wxID_ANY, wxT("&About")); 
 	wxMenuItem* help = new wxMenuItem(nullptr, wxID_ANY, wxT("&How to Play\tCtrl+Shift+?")); 
+	wxMenuItem* showValue = new wxMenuItem(nullptr, wxID_ANY, wxT("&Show Current Value")); 
+	wxMenuItem* regenValue = new wxMenuItem(nullptr, wxID_ANY, wxT("&Regenerate Value")); 
 
 	// Add menu items to menus
 	// Game menu
 	gameMenu->Append(newGame);
 	gameMenu->Append(setMode);
+	gameMenu->AppendSeparator();
+	gameMenu->Append(showValue);
+	gameMenu->Append(regenValue);
 	gameMenu->AppendSeparator();
 	gameMenu->Append(exit);
 	// Help menu
@@ -39,6 +47,8 @@ cMain::cMain()
 	Bind(wxEVT_MENU, &cMain::OnExitButtonClicked, this, exit->GetId());
 	Bind(wxEVT_MENU, &cMain::OnAboutMenuClicked, this, about->GetId());
 	Bind(wxEVT_MENU, &cMain::OnHowToPlayMenuClicked, this, help->GetId());
+	Bind(wxEVT_MENU, &cMain::OnShowCurrentValueMenuClicked, this, showValue->GetId());
+	Bind(wxEVT_MENU, &cMain::OnRegenerateValueMenuClicked, this, regenValue->GetId());
 
 	// Create sizers
 	wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
@@ -49,7 +59,9 @@ cMain::cMain()
 	m_ExitButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnExitButtonClicked, this);
 
 	m_GuessButton = new wxButton(this, wxID_ANY, "Guess", wxPoint(0, 0), wxSize(60, 21));
-	m_GuessInput = new wxTextCtrl(this, wxID_ANY, "Test", wxDefaultPosition, wxSize(175, 20));
+	m_GuessButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnGuessButtonClicked, this);
+	m_GuessInput = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(175, 20));
+	m_GuessInput->SetMaxLength(4);
 	guessInputSizer->Add(new wxStaticText(this, wxID_ANY, "Guess Input: ", wxDefaultPosition, wxDefaultSize), 
 		wxSizerFlags(0).CenterVertical());
 	guessInputSizer->Add(m_GuessInput);
@@ -59,8 +71,8 @@ cMain::cMain()
 	m_ResultsBox = new wxListBox(this, wxID_ANY, wxDefaultPosition, wxSize(310, 150));
 
 	mainSizer->AddSpacer(5);
-	mainSizer->Add(new wxStaticText(this, wxID_ANY, "Bulls and Cows", wxDefaultPosition, wxDefaultSize),
-		0, wxLEFT, 10);
+	m_TitleText = new wxStaticText(this, wxID_ANY, "Bulls and Cows", wxDefaultPosition, wxDefaultSize);
+	mainSizer->Add(m_TitleText, 0, wxLEFT, 10);
 	mainSizer->Add(guessInputSizer, 0, wxALL, 10);
 	mainSizer->Add(new wxStaticText(this, wxID_ANY, "Results:", wxDefaultPosition, wxDefaultSize),
 		0, wxLEFT, 10);
@@ -75,6 +87,9 @@ cMain::cMain()
 	Layout();
 	SetBackgroundColour(wxColour("lightgrey"));
 	CenterOnScreen();
+
+	LoadWords();
+	SetGameMode(GameMode::GAMEMODE_NUMBER);
 }
 
 cMain::~cMain()
@@ -86,12 +101,32 @@ void cMain::SetGameMode(GameMode gameMode)
 	m_CurrentMode = gameMode;
 	m_GuessInput->Clear();
 
-	wxMessageBox(GameModeToCStr(m_CurrentMode));
+	static wxTextValidator numValidator(wxFILTER_INCLUDE_CHAR_LIST);
+	numValidator.SetCharIncludes("0123456789");
+	switch (m_CurrentMode)
+	{
+	case GameMode::GAMEMODE_NUMBER:
+		m_TitleText->SetLabelText("Bulls and Cows: Number Mode");
+		m_GuessInput->SetValidator(numValidator);
+		SelectNumber();
+		break;
+	case GameMode::GAMEMODE_WORD:
+		m_TitleText->SetLabelText("Bulls and Cows: Word Mode");
+		m_GuessInput->SetValidator(wxTextValidator(wxFILTER_ALPHA));
+		SelectWord();
+		break;
+	}
 }
 
 void cMain::OnExitButtonClicked(wxCommandEvent& evt)
 {
 	this->Close();
+	evt.Skip();
+}
+
+void cMain::OnGuessButtonClicked(wxCommandEvent& evt)
+{
+	CheckGuess();
 	evt.Skip();
 }
 
@@ -120,9 +155,44 @@ void cMain::OnHowToPlayMenuClicked(wxCommandEvent& evt)
 	evt.Skip();
 }
 
+void cMain::OnShowCurrentValueMenuClicked(wxCommandEvent& evt)
+{
+	wxMessageBox(m_CurrentValue, "Current Value");
+}
+
+void cMain::OnRegenerateValueMenuClicked(wxCommandEvent& evt)
+{
+	SetGameMode(m_CurrentMode);
+}
+
 void cMain::NewGame()
 {
 	wxMessageBox("New game clicked");
+}
+
+void cMain::CheckGuess()
+{
+	if (m_GuessInput->GetLineText(0).Length() != 4)
+	{
+		// Guess isn't long enough
+		wxMessageBox("Guess should be 4 characters long!", "Error with guess", wxICON_ERROR);
+		return;
+	}
+
+	if (m_CurrentMode == GameMode::GAMEMODE_NUMBER)
+	{
+		std::vector<char> seenChars;
+		for (char c : m_GuessInput->GetLineText(0))
+		{
+			if (std::count(seenChars.begin(), seenChars.end(), c))
+			{
+				// Char has been seen before
+				wxMessageBox("There should be no duplicate digits!", "Error with guess", wxICON_ERROR);
+				return;
+			}
+			seenChars.push_back(c);
+		}
+	}
 }
 
 const char* cMain::GameModeToCStr(const GameMode& gameMode)
@@ -136,4 +206,51 @@ const char* cMain::GameModeToCStr(const GameMode& gameMode)
 	default:
 		return "Invalid";
 	}
+}
+
+void cMain::LoadWords(std::string filename)
+{
+	std::ifstream file(filename);
+	if (!file.is_open())
+	{
+		wxMessageBox("Failed to load words file!", "Error Loading", wxICON_ERROR);
+		return;
+	}
+
+	m_WordPool.clear();
+	std::string line;
+
+	while (std::getline(file, line))
+	{
+		line.erase(std::remove_if(line.begin(), line.end(), [](char c) {
+			return std::isspace(static_cast<unsigned char>(c));
+			}), line.end());
+
+		if (line.length() == 4)
+			m_WordPool.push_back(line);
+		else
+			wxMessageBox("Line not the right size!", "Error Loading", wxICON_ERROR);
+	}
+}
+
+void cMain::SelectNumber()
+{
+	static std::random_device rd;
+	static std::mt19937 engine(rd());
+	std::stringstream ss;
+	std::vector<char> digits{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	for (int i = 0; i < 4; i++)
+	{
+		std::uniform_int_distribution<> distribution(0, digits.size() - 1);
+		int index = distribution(engine);
+		ss << digits.at(index);
+		digits.erase(digits.begin() + index);
+	}
+
+	m_CurrentValue = ss.str();
+}
+
+void cMain::SelectWord()
+{
 }
--- BullsAndCows/cMain.h
+++ BullsAndCows/cMain.h
@@ -18,19 +18,30 @@ public:
 	void SetGameMode(GameMode gameMode);
 
 	void OnExitButtonClicked(wxCommandEvent& evt);
+	void OnGuessButtonClicked(wxCommandEvent& evt);
 	void OnNewGameMenuClicked(wxCommandEvent& evt);
 	void OnSetModeMenuClicked(wxCommandEvent& evt);
 	void OnAboutMenuClicked(wxCommandEvent& evt);
 	void OnHowToPlayMenuClicked(wxCommandEvent& evt);
+	void OnShowCurrentValueMenuClicked(wxCommandEvent& evt);
+	void OnRegenerateValueMenuClicked(wxCommandEvent& evt);
 	void NewGame();
+	void CheckGuess();
 
 	const char* GameModeToCStr(const GameMode& gameMode);
 
+	wxStaticText* m_TitleText = nullptr;
 	wxButton* m_GuessButton = nullptr;
 	wxTextCtrl* m_GuessInput = nullptr;
 	wxListBox* m_ResultsBox = nullptr;
 	wxButton* m_ExitButton = nullptr;
 
 private:
+	void LoadWords(std::string filename = "words.txt");
+	void SelectNumber();
+	void SelectWord();
+
+	std::vector<std::string> m_WordPool;
+	std::string m_CurrentValue = "";
 	GameMode m_CurrentMode = GameMode::GAMEMODE_NUMBER;
 };
